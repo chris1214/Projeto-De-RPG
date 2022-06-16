20fcd1591fa64f55cf8ce1e0d1e3abff3a463e68
christopher <christopher_andrio@hotmail.com>
2022-06-16 03:11:43 -0300

    Adicionando Bootstrap e criando tela de login

--- example-project/src/pages/login.vue
+++ example-project/src/pages/login.vue
@@ -1,5 +1,19 @@
 <template>
-<p>login</p>
+  <div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4 offset-md-4">
+          <div class="card" style="border: 1px solid black">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
